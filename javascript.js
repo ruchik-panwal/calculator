3088697ad91070bcf84d1ffb5b4ef3d5fe860ff0
Ruchik <ruchik.panwal@gmail.com>
2024-06-22 23:51:31 +0530

    done something happened something

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ const button = document.querySelectorAll("button");
 const display = document.querySelector(".display");
 const pos = document.querySelector(".pos");
 let num1 = "", num2 = 0, op = 0;
-let toggle = 0;
+let toggle = 0, count = 0;
 
 // The translucent signs on the screen
 
@@ -17,6 +17,7 @@ const root = document.querySelector(".rootScreen");
 const pow = document.querySelector(".powScreen");
 const equal = document.querySelector(".equalScreen");
 const screenSign = document.querySelectorAll("#screenSign");
+const posF = document.querySelector(".posFlex");
 
 array = [];
 
@@ -26,11 +27,31 @@ function input() {
     button.forEach((button) => {
         button.addEventListener('mousedown', () => {
 
-            if(toggle == 1){
+            if (button.id == "ac") {
+                num1 = num2 = 0;
+                display.textContent = "";
+                pos.textContent = "00";
+                toggle = 1;
+                count = -1;
+
+            }
+
+
+            if (toggle == 1) {
                 display.textContent = "";
                 toggle = 0;
-                
-                screenSign.forEach((screenSign) => screenSign.setAttribute("style", "opacity : 0.1;"))
+
+                screenSign.forEach((screenSign) => screenSign.setAttribute("style", "opacity : 0.1;"));
+                if (count < 5) {
+                    count++;
+                    posF.setAttribute("style", "opacity : 1;");
+                }
+                pos.textContent = "0" + count;
+
+                if (pos.textContent == "00")
+                    posF.setAttribute("style", "opacity : 0.1;");
+
+
             }
 
             for (let i = 0; i < 10; i++) {
@@ -43,9 +64,6 @@ function input() {
                 display.textContent = (display.textContent).slice(0, (display.textContent.length - 1));
             }
 
-            if (button.id == "ac") {
-                display.textContent = "";
-            }
 
             if (button.id == "point") {
                 if (display.textContent.includes(".")) {
@@ -55,7 +73,6 @@ function input() {
                     display.textContent += ".";
             }
 
-
             if (button.id == "nSign") {
                 if (display.textContent.includes("-")) {
                     display.textContent = (display.textContent).slice(1, (display.textContent.length));
@@ -64,7 +81,7 @@ function input() {
                     display.textContent = "-" + display.textContent;
             }
 
-            if (button.className == "operator") {
+            if (button.className == "operator" || button.className == "smallMath") {
 
                 if (num1 != "") {
                     num2 = display.textContent;
@@ -74,48 +91,65 @@ function input() {
                 }
                 else {
                     num1 = display.textContent;
-                    console.log(num1);
                     op = button.id;
                 }
 
-                if(button.id == "add"){
-                    plus.setAttribute("style", "opacity : 1;")
-                }
-                if(button.id == "min"){
-                    minus.setAttribute("style", "opacity : 1;")
-                }
-                if(button.id == "div"){
-                    divide.setAttribute("style", "opacity : 1;")
-                }
-                if(button.id == "mul"){
-                    cross.setAttribute("style", "opacity : 1;")
-                }
-                if(button.id == "equal"){
-                    equal.setAttribute("style", "opacity : 1;")
-                }
+                screenToggle(button.id);
                 toggle = 1;
+
+                console.log(num1);
+                console.log(num2);
             }
         });
     });
 }
 
+function screenToggle(sign) {
+
+
+    if (sign == "add") 
+        plus.setAttribute("style", "opacity : 1;");
+    
+    if (sign == "min") 
+        minus.setAttribute("style", "opacity : 1;");
+    
+    if (sign == "div") 
+        divide.setAttribute("style", "opacity : 1;");
+    
+    if (sign == "mul") 
+        cross.setAttribute("style", "opacity : 1;");
+    
+    if (sign == "equal") 
+        equal.setAttribute("style", "opacity : 1;");
+    
+    if (sign == "pow") 
+        pow.setAttribute("style", "opacity : 1;");
+    
+    if(sign == "sqrt")
+        root.setAttribute("style", "opacity : 1;");
+
+}
 
 function operate(n1, n2, op) {
 
-    if (op === "add") {
+    if (op === "add")
         return n1 + n2;
-    }
 
-    if (op === "min") {
+    if (op === "min")
         return n1 - n2;
-    }
 
-    if (op === "mul") {
+    if (op === "mul")
         return n1 * n2;
-    }
 
-    if (op === "div") {
+    if (op === "div")
         return n1 / n2;
-    }
+
+    if (op === "equal")
+        return num1;
+
+    if (op === "pow")
+        return Math.pow(num1, num2);
+
+
 }
 
